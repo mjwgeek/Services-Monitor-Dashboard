ceae4eafcd5d8587e5fcb3556daba79ad45904ea
Mike Webb <83971391+mjwgeek@users.noreply.github.com>
2025-05-12 20:05:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -97,8 +97,6 @@ sed -i "1s|^#!/usr/bin/env python3|#!/$VENV_BIN/python3|" "$APP_DIR/prefetch_ser
 
 # 9. Test run prefetch_services.py with the virtual environment.
 echo "[*] Test running prefetch_services.py"
-# Try explicitly setting the PYTHONPATH
-#PYTHONPATH="$VENV_DIR/lib/python3.11/site-packages" "$VENV_BIN/python3" "$APP_DIR/prefetch_services.py"
 "$VENV_BIN/python3" "$APP_DIR/prefetch_services.py"
 
 # 10. Done
